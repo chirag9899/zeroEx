cb4f542b229ec22c5820455dd04c6dc48f5b9086
Chirag <chirag0264@gmail.com>
2024-05-31 18:40:44 +0530

    withdraw added

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -9,12 +9,21 @@ import { useContract } from "../providers/thirdwebHook";
 const HomePage = () => {
 
 
-  const {contractInstance, userBalance}: any =  useContract()
+  const {deposit, userBalance, getBalance, withdraw}: any =  useContract()
   
-  console.log(userBalance)
+  const data  = async() => {
+    const bal =  await getBalance()
+    console.log(bal)
+  }
 
   return (
     <div className="ml-[6%] mr-[30%] p-4 ">
+      <div className="flex gap-10">
+      <button onClick={data}>balance</button>
+      <button onClick={async() => await deposit(false, 1000)}>deposit</button>
+      <button onClick={async() => await withdraw(false, 1000)}>withdraw</button>
+
+      </div>
       <LSidebar />
       <div className="grid grid-cols-2 gap-4">
         <Balance />
--- frontend/src/providers/thirdwebHook.tsx
+++ frontend/src/providers/thirdwebHook.tsx
@@ -3,7 +3,9 @@ import { arbitrumSepolia } from "thirdweb/chains";
 import { createThirdwebClient, getContract, readContract, prepareContractCall, sendAndConfirmTransaction, ThirdwebContract, PreparedTransaction } from "thirdweb";
 import { useActiveAccount, useSendTransaction } from 'thirdweb/react';
 import {abi as executerAbi} from "../abi/executerAbi.ts";
+import { abi as usdcAbi } from '../abi/usdcAbi.ts';
 import { createWallet, injectedProvider, Wallet, walletConnect } from "thirdweb/wallets";
+
 
 interface ContractContextState {
   contractInstance: any; 
@@ -15,6 +17,7 @@ const ContractContext = createContext<ContractContextState | undefined>(undefine
 export const ContractProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     // <Readonly<ThirdwebContract<Abi>>>(null)
   const [contractInstance, setContractInstance] = useState<ThirdwebContract>();
+  const [usdcTokenInstance, setUsdcTokenInstance] = useState<ThirdwebContract>();
 
   const [userBalance, setUserBalance] = useState<{ETH: number, USDC: number}>({ETH: 0, USDC: 0});
   const activeAccount = useActiveAccount();
@@ -33,9 +36,16 @@ export const ContractProvider: React.FC<{ children: React.ReactNode }> = ({ chil
                 client: client,
                 chain: arbitrumSepolia
             });
-            console.log("hello",contract)
+
+            const usdc_contract =  getContract({
+                address: "0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d", 
+                abi: usdcAbi as any,
+                client: client,
+                chain: arbitrumSepolia
+            });
 
             setContractInstance(contract);
+            setUsdcTokenInstance(usdc_contract)
         } catch (error) {
             console.error(error);
             
@@ -52,43 +62,136 @@ export const ContractProvider: React.FC<{ children: React.ReactNode }> = ({ chil
 
         const balance = await readContract({
             contract: contractInstance,
-            method: "function getUserOrders(address) view returns ((uint256, uint256))",
-            params: ["0xE2db7ef93684d06BbF47137000065cF26E878B2e"],
+            method: "function getUserBalance(address) view returns ((uint256, uint256))",
+            params: [activeAccount?.address?.toString() || '0xe2db7ef93684d06bbf47137000065cf26e878b2e'],
         });
+        console.log(balance)
+        setUserBalance({ ETH: Number(balance[0]), USDC: Number(balance[1]) });
+        return balance
+    };
+
+
+    const getUserOrder = async () => {
+        if (!contractInstance) return; 
+
+        const balance = await readContract({
+            contract: contractInstance,
+            method: "function getUserOrder(address) view returns ((uint256, uint256))",
+            params: [activeAccount?.address?.toString() || '0xe2db7ef93684d06bbf47137000065cf26e878b2e'],
+        });
+        console.log(balance)
         setUserBalance({ ETH: Number(balance[0]), USDC: Number(balance[1]) });
         return balance
     };
 
 const deposit = async(isEth: boolean ,amount: number) => {
-    if (!activeAccount) {
-        throw new Error('Active account is undefined');
-    }
-
-    if (!contractInstance) {
-        throw new Error('Contract instance is undefined');
-    }
-    let transaction: PreparedTransaction<any> ;
-
-    transaction = prepareContractCall({
-        contract: contractInstance,
-        method: "function depositFunds(isEth ,address) public payable",
-        params: [  isEth ? true : false , amount.toString()],
-        value: isEth ? BigInt(amount) : BigInt(0) 
-    });
-
-    var result = await sendAndConfirmTransaction({
-        transaction: transaction,
-        account: activeAccount,
-    });
+    console.log(isEth, amount)
+    try {
+        if (!activeAccount) {
+            throw new Error('Active account is undefined');
+        }
     
-    return result;
+        if (!contractInstance) {
+            throw new Error('Contract instance is undefined');
+        }
+     
+        let transaction: PreparedTransaction<any> ;
+    
+        if (!isEth) {
+            console.log("enter")
+            if (!usdcTokenInstance) {
+                throw new Error('Contract instance is undefined');
+            }
+    
+            let allowance_transaction = await readContract({
+                contract: usdcTokenInstance,
+                method: "function allowance(address _owner, address _spender) public view returns (uint256 remaining)",
+                params: [ activeAccount.address , contractInstance.address],
+                value: isEth ? BigInt(amount) : BigInt(0) 
+            });
+            
+            console.log(allowance_transaction)
+    
+              if (Number(allowance_transaction) < amount) {
+
+                let aprove_tx = prepareContractCall({
+                    contract: usdcTokenInstance,
+                    method: "function approve(address _spender, uint256 _value) public returns (bool success)",
+                    params: [  contractInstance.address , BigInt(amount)],
+                    value: isEth ? BigInt(amount) : BigInt(0) 
+                });
+
+                var aprove_result = await sendAndConfirmTransaction({
+                    transaction: aprove_tx,
+                    account: activeAccount,
+                });
+
+                if (aprove_result.status === "reverted") {
+                    throw new Error("Aprove transaction failed")
+                } 
+                
+              }
+            }
+    
+        transaction = prepareContractCall({
+            contract: contractInstance,
+            method: "function depositFunds(bool isEth ,uint256 amount) public payable",
+            params: [  (isEth ? true : false) , BigInt(amount)],
+            value: isEth ? BigInt(amount) : BigInt(0) 
+        });
+    
+        var result = await sendAndConfirmTransaction({
+            transaction: transaction,
+            account: activeAccount,
+        });
+        
+        return result;
+
+    } catch (error) {
+        console.log({error})
+        throw new Error(`Error depositing ${error}`)
+    }
+   
+}
+
+const withdraw = async(isEth: boolean, amount: number) => {
+    console.log(isEth, amount)
+    try {
+        if (!activeAccount) {
+            throw new Error('Active account is undefined');
+        }
+
+        if (!contractInstance) {
+            throw new Error('Contract instance is undefined');
+        }
+
+        let transaction: PreparedTransaction<any> ;
+
+        transaction = prepareContractCall({
+            contract: contractInstance,
+            method: "function withdrawFunds(uint256 amount, bool isETH) public",
+            params: [BigInt(amount), isEth],
+            value: BigInt(0)  
+        });
+
+        const result = await sendAndConfirmTransaction({
+            transaction: transaction,
+            account: activeAccount,
+        });
+
+        return result;
+
+    } catch (error) {
+        console.log({error});
+        throw new Error(`Error withdrawing: ${error}`);
+    }
 }
 
 
 
 
   return (
-    <ContractContext.Provider value={{ contractInstance, userBalance } as any}>
+    <ContractContext.Provider value={{ contractInstance, userBalance, deposit, getBalance , withdraw} as any}>
       {children}
     </ContractContext.Provider>
   );
